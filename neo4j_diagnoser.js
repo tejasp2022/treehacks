41ad010c11666b6cc270686e0505eabe7ffe1460
ens36 <eswecker@andrew.cmu.edu>
2020-02-15 14:53:39 -0800

    Added get nodes function

--- neo4j_diagnoser.js
+++ neo4j_diagnoser.js
@@ -1,9 +1,24 @@
-var neo4j = require('neo4j-driver').v1;
+var neo4j = require('neo4j-driver');
 
 var driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'hellohello'));
 var session = driver.session();
 
-function addGroup(group) {
+var symptoms = new Set();
+var poss_diseases = new Set();
+var ign_diseases = new Set();
+
+async function get_nodes(node_type) {
+    const result = await session
+        .run("MATCH (n:'" + node_type + "') RETURN n.name")
+        .catch(function (err){
+            console.log("Error: " + err);
+        })
+        .then(function (){
+            console.log("Success");
+        });
+};
+/*
+function process_first_symptom(symptom) {
 	session
            .run('CREATE (n:Group { groupName: \'' + group.groupName + '\'})')
            .catch(function (err) {
@@ -13,3 +28,7 @@ function addGroup(group) {
                console.log('success')
            });
 }
+*/
+
+get_nodes("Symptom");
+driver.close();
